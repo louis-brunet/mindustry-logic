a139db6602ffe1cba2ddcb1329b3a7bd4872e3ea
Louis <123louisb@gmail.com>
2023-05-08 12:01:31 +0200

    only write coords if status != 0

--- src/scripts/enemy-radar-result-propagator.ts
+++ src/scripts/enemy-radar-result-propagator.ts
@@ -6,13 +6,17 @@ const memIn = new Memory(cellIn);
 const memOut = new Memory(cellOut);
 
 const [status, x, y] = memIn;
-memOut[0] = status;
-memOut[1] = x;
-memOut[2] = y;
 
-print`Wrote:\n`;
+print`Writing:\n`;
 print`status=[accent]${status}[]\n`;
-print`x=[accent]${x}[]\n`;
-print`y=[accent]${y}[]`;
-// print`(x, y)=[accent](${x}, ${y})[]\n`;
+
+memOut[0] = status;
+if (status !== 0) {
+    print`x=[accent]${x}[]\n`;
+    print`y=[accent]${y}[]`;
+
+    memOut[1] = x;
+    memOut[2] = y;
+}
+
 printFlush(msgDebug);
