24bcb25fe5e3302ed758f01962acf959bd2fb160
Louis <123louisb@gmail.com>
2023-05-08 11:56:55 +0200

    debug logs in result propagator

--- src/scripts/enemy-radar-result-propagator.ts
+++ src/scripts/enemy-radar-result-propagator.ts
@@ -1,5 +1,7 @@
 let cellIn = getBuilding('cell1');
 let cellOut = getBuilding('cell2');
+let msgDebug = getBuilding('message1');
+
 const memIn = new Memory(cellIn);
 const memOut = new Memory(cellOut);
 
@@ -7,3 +9,10 @@ const [status, x, y] = memIn;
 memOut[0] = status;
 memOut[1] = x;
 memOut[2] = y;
+
+print`Wrote:\n`;
+print`status=[accent]${status}[]\n`;
+print`x=[accent]${x}[]\n`;
+print`y=[accent]${y}[]`;
+// print`(x, y)=[accent](${x}, ${y})[]\n`;
+printFlush(msgDebug);
